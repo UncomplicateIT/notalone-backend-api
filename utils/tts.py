ed6dc4e48f9439257cec012e2a9bdd505b9ebca6
Herumb Shandilya <43719685+krypticmouse@users.noreply.github.com>
2023-03-24 23:50:54 +0530

    Add files via upload

--- utils/tts.py
+++ utils/tts.py
@@ -1,29 +1,24 @@
-import torch
-import soundfile as sf
+import base64
+import banana_dev as banana
 
-from datasets import load_dataset
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
-
+# Run the model
 class TTSHandler:
     def __init__(self) -> None:
-        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
-
-        self.embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+        self.api_key = "656ac1d7-8902-4193-9e5c-c1504aa7b1d9"
+        self.model_key = "72fde273-52ac-460b-a924-df09dd0ad781"
     
     def get_audio_from_text(self, text):
-        inputs = self.__prepare_inputs(text)
-        speaker_embeddings = torch.tensor(self.embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+        inputs = {
+            "text": text,
+        }
 
-        speech = self.model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=self.vocoder)
+        speech = banana.run(self.api_key, self.model_key, inputs)
         return self.__prepare_output(speech)
 
-    def __prepare_inputs(self, text):
-        return self.processor(text=text, 
-                         return_tensors="pt"
-        )
-    
     def __prepare_output(self, speech):
-        sf.write("speech.wav", speech.numpy(), samplerate=16000)
+        encoded_bytes = speech['modelOutputs'][0]['audio'].split(',')[1].encode("ascii")
+        decoded_bytes = base64.decodebytes(encoded_bytes)
+
+        with open("temp.mp3", "wb") as mp3_file:
+            mp3_file.write(decoded_bytes)
         return "speech.wav"
