1bc95a4d6ff5cb87762ab6dc6e1ecb46eb45c07d
“krypticmouse” <herumbshandilya123@gmail.com>
2023-03-25 00:33:33 +0530

    finalize api

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 
 from utils.tts import TTSHandler
@@ -10,22 +10,30 @@ tts = TTSHandler()
 whisper = WhisperHandler()
 chatgpt = ChatGPTHandler()
 
-@app.get("/")
+@app.middleware("http")
+async def addmiddleware(request: Request, call_next):
+   print("Middleware works!")
+   response = await call_next(request)
+   return response
+
+@app.post("/")
 async def root():
     return "NotAlone Backend API"
 
-@app.get("/chatgpt")
+@app.post("/chatgpt")
 async def chatgpt_route(
     text: str, 
     action: str
 ):
     return chatgpt.get_text_from_input(text, action)
 
-@app.get("/whisper")
-async def whisper_route():
+@app.post("/whisper")
+async def whisper_route(
+    token: str
+):
     pass
 
-@app.get("/tts")
+@app.post("/tts")
 async def tts_route(text: str):
     return FileResponse(tts.get_audio_from_text(text))
     
--- utils/chatgpt.py
+++ utils/chatgpt.py
@@ -4,7 +4,7 @@ import openai
 
 class ChatGPTHandler:
     def __init__(self) -> None:
-        openai.api_key = "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"
+        openai.api_key = "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"
     
     def get_text_from_input(self, user_query, action='continue'):
         prompts = self.__fetch_prompts(user_query, action)
@@ -16,12 +16,14 @@ class ChatGPTHandler:
             max_tokens = 512
         )
         
-        return self.__prepare_output(response)
+        return self.__prepare_output(user_query, response, action="continue")
 
     def __fetch_prompts(self, user_query, action):
         conv_list = json.load(open(f'utils/prompts/{action}.json', 'r'))
         conv_list.append({"role": "user", "content": user_query})
         return conv_list
 
-    def __prepare_output(self, response):
+    def __prepare_output(self, user_query, response, action):
+        if action == 'continue':
+            return user_query.strip() + " " + response['choices'][0]['message']['content']
         return response['choices'][0]['message']['content']
